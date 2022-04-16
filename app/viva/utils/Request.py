f9613045202f424e4f721f227ac6846edec911b6
duvanjm <duvanjarin@gmail.com>
2022-04-15 20:31:46 -0500

    remove comments

--- app/viva/utils/Request.py
+++ app/viva/utils/Request.py
@@ -19,15 +19,9 @@ class Request():
 
     def req(self, idx, n_max):
         """request hackerank API"""
-        #lst = []
         res = cache.get('full_lst')
         return res[idx:n_max]
 
-        """ res = res[idx:]
-        for i in res[:n_max]:
-            r = requests.get('https://hacker-news.firebaseio.com/v0/item/' + str(i) +'.json?print=pretty').json()
-            lst.append({'ID': r.get('id'), 'titulo': r.get('title')})
-        return lst """
 
     def req_one(self, idx, n_max):
         """request hackerank API"""
