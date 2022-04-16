d7e42047295c046a1b4e109b572e7f7bb0a0f6d6
duvanjm <duvanjarin@gmail.com>
2022-04-15 20:35:25 -0500

    fix pep8

--- app/viva/utils/Request.py
+++ app/viva/utils/Request.py
@@ -12,16 +12,14 @@ class Request():
         lst = []
         res = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').json()
         for i in res:
-            r = requests.get('https://hacker-news.firebaseio.com/v0/item/' + str(i) +'.json?print=pretty').json()
+            r = requests.get('https://hacker-news.firebaseio.com/v0/item/' + str(i) + '.json?print=pretty').json()
             lst.append({'ID': r.get('id'), 'titulo': r.get('title')})
         cache.set('full_lst', lst)
-
 
     def req(self, idx, n_max):
         """request hackerank API"""
         res = cache.get('full_lst')
         return res[idx:n_max]
-
 
     def req_one(self, idx, n_max):
         """request hackerank API"""
@@ -29,6 +27,6 @@ class Request():
         res = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').json()
         res = res[idx:]
         for i in res[:n_max]:
-            r = requests.get('https://hacker-news.firebaseio.com/v0/item/' + str(i) +'.json?print=pretty').json()
+            r = requests.get('https://hacker-news.firebaseio.com/v0/item/' + str(i) + '.json?print=pretty').json()
             lst.append({'ID': r.get('id'), 'titulo': r.get('title')})
         return lst
